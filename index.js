c987170d52597719925b7e41ad2a514c79acffb8
Mkrah <amirou.kaba26@compscihigh.org>
2026-03-17 19:20:04 +0000

    Finished

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { stringify } = require("node:querystring");
 
 // import mongoose using require
 
@@ -16,9 +18,23 @@ app.use((req, res, next) => {
 
 // create a studentSchema with name, grade, advisory, and fav subject
 
+const studentSchema = new mongoose.Schema({
+  name: { type: String, required: true  },
+  grade: {type: Number, required: true, default: 9 , min: 9, max: 12},
+  advisory: {type: String, required: true }, 
+  favSubject: {type: String, default: "No favorite subject" },
+})
+
 // connect your schema to a model called Student
 
+const Student = mongoose.model("Student", studentSchema, "Students")
+
 // create a route hanlder for /g12 that returns every student in grade 12
+
+app.get("/g12", async (req, res) => {
+  const students = await Student.find({grade: 12})
+  res.json(students)
+})
 
 // create a route hanlder for /me that returns yourself without using your name
 
@@ -32,7 +48,12 @@ async function startServer() {
   );
 
   // before you run your code, uncomment the following and add your info, then the info of a friend
-  //   await new ...({...}).save();
+  //  await new Student({
+  //   name: "Amirou Kaba",
+  //   grade: 12,
+  //   advisory: "Benevolent Lotuses",
+  //   favSubject: "None"
+  //  }).save();
 
   app.listen(3000, () => {
     console.log(`Server is running!`);
